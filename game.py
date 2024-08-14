ea329428f306088da4b9b2456d9b47076e0594d5
Josh578997 <jval0014@student.monash.edu>
2024-08-14 14:41:13 +1000

    sdffsdffsd

--- game.py
+++ game.py
@@ -301,11 +301,11 @@ class Game:
                     hand_card = self.current_player.hand[i]   # card to be played
                     hand_card_color = hand_card.color
                     hand_card_label = hand_card.label
-                else:
-                    new_card = self.draw_card(self.current_player, playing = True)    
-                    if new_card is not None:
-                        self.discard_pile.push(new_card)
-                        continue
+            if hand_card_color == None and hand_card_label == None:
+                new_card = self.draw_card(self.current_player, playing = True)    
+                if new_card is not None:
+                    self.discard_pile.push(new_card)
+                    continue
             if hand_card_label == CardLabel.CRAZY:
                 played_card = self.current_player.play_card(0)
                 self.crazy_play(played_card)
