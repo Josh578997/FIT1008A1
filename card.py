650483c17b3f8624d1bf0b2ae3172f3f16640ec3
Josh578997 <jval0014@student.monash.edu>
2024-08-14 13:05:35 +1000

    sdfsdfdfsdfssdf

--- card.py
+++ card.py
@@ -53,7 +53,7 @@ class Card:
         self.label = label
 
     def __lt__(self,other):
-        return (self.color < other.color)
+        return (self.color < other.color) and self.label < other.label
         
     def __le__(self,other):
-        return (self.color <= other.color)
+        return (self.color <= other.color) and self.label <= other.label
