f00f1be0e64e277d13983e0be901fd5c4e488cf5
Josh578997 <jval0014@student.monash.edu>
2024-08-16 13:30:37 +1000

    nbkbhjkh

--- game.py
+++ game.py
@@ -357,12 +357,12 @@ class Game:
                 self.current_label = new_card.label
                 self.discard_pile.push(new_card)
                 continue
-            elif new_card_label == CardLabel.SKIP and new_card_color == self.current_color:
+            elif new_card_label == CardLabel.SKIP and (new_card_color == self.current_color or new_card_label == self.current_label):
                 self.play_skip()
                 self.current_color = new_card_color
                 self.current_label = new_card_label
                 self.discard_pile.push(new_card)
-            elif new_card_label == CardLabel.REVERSE and new_card_color == self.current_color:
+            elif new_card_label == CardLabel.REVERSE and (new_card_color == self.current_color or new_card_label == self.current_label):
                 self.play_reverse()
                 self.current_color = new_card_color
                 self.current_label = new_card_label
